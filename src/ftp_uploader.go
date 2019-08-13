e7dc626cac445685a27e42ed46fc40bf3c57fc2c
tuitong4 <zifeng456@qq.com>
2019-08-14 00:55:08 +0800

    Create ftp_uploader.go

---
+++ src/ftp_uploader.go
@@ -0,0 +1,307 @@
+package main
+
+import (
+    "bufio"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "nwssh"
+    "os"
+    "strings"
+    "sync"
+    "time"
+)
+
+type Args struct {
+    hostfile      string
+    username      string
+    password      string
+    ftpserver     string
+    filename      string
+    ftpuser       string
+    ftppassword   string
+    vpn           string
+    logdir        string
+    transporttime int
+    swvendor      string
+}
+
+var args = Args{}
+
+func initflag() {
+    flag.StringVar(&args.hostfile, "f", "", `Target hosts list file, each ip on a separate line, for example:
+'10.10.10.10'
+'12.12.12.12'.`)
+    flag.StringVar(&args.swvendor, "V", "", `Vendor of target host, if not spicified, it will be checked 
+automatically.`)
+    flag.StringVar(&args.username, "u", "", "Username for login.")
+    flag.StringVar(&args.password, "p", "", "Password for login.")
+    flag.StringVar(&args.ftpserver, "ftpsrv", "", "Ftp Server Address.")
+    flag.StringVar(&args.filename, "file", "", "Filename to upload.")
+    flag.StringVar(&args.ftpuser, "ftpuser", "ftp-network", "Ftp Username.")
+    flag.StringVar(&args.ftppassword, "ftppwd", "", "Ftp PassWord.")
+    flag.StringVar(&args.vpn, "vpn", "", "Vpn instance name.")
+    flag.StringVar(&args.logdir, "logpath", "", "Log command output to /<path>/<ip_addr> instead of stdout.")
+    flag.IntVar(&args.transporttime, "transporttime", 300, "Time of uploading file complete, in second.")
+    flag.Parse()
+}
+func h3c_upload(host, port string, sshoptions nwssh.SSHOptions, args Args) (string, bool) {
+
+    var devssh *nwssh.SSHBase
+    var err error
+
+    username := args.username
+    password := args.password
+
+    devssh, err = nwssh.SSH(host, port, username, password, time.Duration(10)*time.Second, sshoptions)
+    defer devssh.Close()
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return "", false
+    }
+    if err = devssh.Connect(); err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return "", false
+    }
+
+    var device nwssh.SSHBASE
+
+    device = &nwssh.H3cSSH{devssh}
+
+    if !device.SessionPreparation() {
+        log.Printf("[%s]Failed init execute envirment. Try to exectue command directly.", host)
+    }
+
+    ftp_cmd := ""
+    if args.vpn != "" {
+        ftp_cmd = `ftp ` + args.ftpserver + ` vpn-instance ` + args.vpn + ` source ip ` + host
+    } else {
+        ftp_cmd = `ftp ` + args.ftpserver + ` source ip ` + host
+    }
+
+    output := ""
+    o, err := device.ExecCommandExpect(ftp_cmd, "none)):", time.Second*5)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+
+    output += o
+    o, err = device.ExecCommandExpect(args.ftpuser, "assword:", time.Second*5)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+    output += o
+    o, err = device.ExecCommandExpect(args.ftppassword, "ftp>", time.Second*5)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+
+    output += o
+    o, err = device.ExecCommandExpect("binary", "ftp>", time.Second*5)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+
+    output += o
+    o, err = device.ExecCommandExpectPrompt("get "+args.filename, time.Second*time.Duration(args.transporttime))
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+    output += o
+    o, err = device.ExecCommandExpectPrompt("quit", time.Second*3)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+    output += o
+    return output, false
+}
+
+func huawei_upload(host, port string, sshoptions nwssh.SSHOptions, args Args) (string, bool) {
+
+    var devssh *nwssh.SSHBase
+    var err error
+
+    username := args.username
+    password := args.password
+
+    devssh, err = nwssh.SSH(host, port, username, password, time.Duration(10)*time.Second, sshoptions)
+    defer devssh.Close()
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return "", false
+    }
+    if err = devssh.Connect(); err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return "", false
+    }
+
+    var device nwssh.SSHBASE
+
+    device = &nwssh.HuaweiSSH{devssh}
+
+    if !device.SessionPreparation() {
+        log.Printf("[%s]Failed init execute envirment. Try to exectue command directly.", host)
+    }
+    ftp_cmd := ""
+    if args.vpn != "" {
+        ftp_cmd = `ftp -a ` + host + ` ` + args.ftpserver + ` vpn-instance ` + args.vpn
+    } else {
+        ftp_cmd = `ftp -a ` + host + ` ` + args.ftpserver
+    }
+
+    output := ""
+    o, err := device.ExecCommandExpect(ftp_cmd, "none)):", time.Second*5)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+    output += o
+    o, err = device.ExecCommandExpect(args.ftpuser, "assword:", time.Second*25)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+    output += o
+    o, err = device.ExecCommandExpect(args.ftppassword, "ftp]", time.Second*25)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+
+    output += o
+    o, err = device.ExecCommandExpect("binary", "ftp]", time.Second*5)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+
+    output += o
+    o, err = device.ExecCommandExpect("get "+args.filename, "ftp]", time.Second*time.Duration(args.transporttime))
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+    output += o
+    o, err = device.ExecCommandExpectPrompt("quit", time.Second*3)
+    if err != nil {
+        log.Printf("[%s]%v\n", host, err)
+        return output + o, false
+    }
+    output += o
+    return output, false
+}
+
+func readlines(filename string) ([]string, error) {
+    file, err := os.Open(filename)
+    if err != nil {
+        return nil, err
+    }
+    reader := bufio.NewReader(file)
+    var lines []string
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            break
+        }
+        lines = append(lines, strings.TrimSpace(line))
+    }
+
+    return lines, nil
+}
+
+func writefile(file, conntent string) error {
+    return ioutil.WriteFile(file, []byte(conntent), 0666)
+}
+
+func main() {
+
+    initflag()
+    if args.username == "" {
+        fmt.Println("Username is expected but got none. See help docs.")
+        os.Exit(0)
+    }
+
+    if args.swvendor == "" {
+        fmt.Println("Switch vendor file is expected but got none. See help docs.")
+        os.Exit(0)
+    }
+
+    if args.ftpserver == "" {
+        fmt.Println("FtpServer is expected but got none. See help docs.")
+        os.Exit(0)
+    }
+
+    if args.filename == "" {
+        fmt.Println("Filename is expected but got none. See help docs.")
+        os.Exit(0)
+    }
+
+    if args.ftppassword == "" {
+        fmt.Println("Please input the ftp server password:")
+        fmt.Scanf("%s", &args.ftppassword)
+    }
+
+    if args.password == "" {
+        fmt.Printf("Please input the password:")
+        fmt.Scanf("%s", &args.password)
+    }
+
+    sshoptions := nwssh.SSHOptions{
+        IgnorHostKey: true,
+        BannerCallback: func(msg string) error {
+            return nil
+        },
+        TermType:     "vt100",
+        TermHeight:   560,
+        TermWidht:    480,
+        ReadWaitTime: time.Duration(500) * time.Millisecond, //Read data from a ssh channel timeout
+    }
+
+    var err error
+
+    hostfile := args.hostfile
+    hosts, err := readlines(hostfile)
+    if err != nil {
+        log.Fatal("%v", err)
+    }
+
+    maxThread := 50
+    threadchan := make(chan struct{}, maxThread)
+
+    wait := sync.WaitGroup{}
+
+    vendor := strings.ToLower(args.swvendor)
+
+    for _, host := range hosts {
+        wait.Add(1)
+        go func(host string) {
+            threadchan <- struct{}{}
+            output := ""
+            if  vendor == "h3c" {
+                output, _ = h3c_upload(host, "22", sshoptions, args)
+            } else if vendor == "huawei" {
+                output, _ = huawei_upload(host, "22", sshoptions, args)
+            } else{
+                fmt.Printf("unspported vendor %s.\n", vendor)
+            }
+
+            if args.logdir == "" {
+                fmt.Printf("--------%s-------\n%s\n", host, output)
+            } else {
+                writefile(args.logdir+host, output)
+            }
+
+            <-threadchan
+            wait.Done()
+        }(host)
+    }
+    wait.Wait()
+
+}
